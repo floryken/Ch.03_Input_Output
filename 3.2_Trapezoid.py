174c809321fb26ddd6c30651dc538d0c5cb1faf4
floryken <floryken@urbandaleschools.com>
2019-09-23 14:08:48 -0500

    a committ

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,4 +10,9 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
-print()
+print("Hello!")
+b1=int(input("What is the First Base of the Trapezoid?"))
+b2=int(input("What is the Second Base of the Trapezoid?"))
+h=int(input("What is the Height of the Trapezoid?"))
+print("Give me one second...")
+print(((b1+b2)/2)*h)
