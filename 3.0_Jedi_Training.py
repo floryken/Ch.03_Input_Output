dfd5aec4a30268a8e0367ef6f0a2b3d4de6ee6e4
floryken <floryken@urbandaleschools.com>
2019-09-23 13:53:59 -0500

    a committ

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,20 +1,24 @@
-#Sign your name:________________
+#Sign your name: Kenny Flory
 
 
 # 1.) Write a program that asks someone for their name and then prints their name to the screen?
-
+Fn=input("What is your Full Name?")
+print("Hello",Fn,"Nice to Meet You")
 
 # 2. Write a a program where a user enters a base and height and you print the area of a triangle.
-
-
+b=int(input("What is the Base of the Triangle?"))
+h=int(input("What is the Height of the Triangle?"))
+print("The area of the triangel is", (b*h)/2)
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
-
+r=int(input("What is the Radius of the Circle?"))
+print("The Circumference of the Circle is", 6.28*r)
 # 4. Ask a user for an integer and then print the square root.
-
-
+s=int(input("What number do you Want the Square Root of?"))
+print("The Square Root of", s, "is", s**.5)
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
-
+m=int(input("What is the Mass of the object?"))
+a=int(input("What is the acceleration?"))
+print("The Midichlorians are off the charts! The Force is strong with this object:", m*a)
 
 
