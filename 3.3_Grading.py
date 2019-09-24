b5b691eb1160c29aa31c2e797d9d377602118689
floryken <floryken@urbandaleschools.com>
2019-09-24 14:01:27 -0500

    a committ

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,9 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+SG=int(input("What is the Semester Grade?"))
+FE=int(input("What was your Final Exam Grade?"))
+EW=int(input("What Weight is the Exam Worth?"))
+p=EW/100
+s=1-p
+print("Your Overall grade is",((SG*s)+(p*FE)))
